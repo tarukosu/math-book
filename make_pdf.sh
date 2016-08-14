37e3ad0af3597b8048096349d8973229b5bac66f
tarukosu <u.au.sky.future12@gmail.com>
2016-08-14 12:42:11 +0900

    update

---
+++ make_pdf.sh
@@ -0,0 +1,1 @@
+pandoc --latex-engine=xelatex -H tex/header.tex -o test.pdf README.md SUMMARY.md chapter1/*.md chapter2/*.md chapter3/*.md
---
+++ utils/md2html.sh
@@ -0,0 +1,24 @@
+#filename="mistake"
+filename=$1
+sed -e "s/\\$\\$\(\\\\[^$]*\)\\$\\$/[tex:{\\\\\1}]/g" $filename.md > $filename.md.tmp
+sed -i -e "s/\\$\\$\([^$]*\)\\$\\$/[tex:{\1}]/g" $filename.md.tmp
+cat $filename.md.tmp
+loop_num=$(cat ${filename}.md.tmp | grep -o \\$\\$ | wc -l)
+echo $loop_num
+for i in `seq 1 $loop_num`
+do
+    echo $i
+    sed -i -e "1,/\\$\\$/s/\\$\\$/[tex:{ \\\\displaystyle /g" $filename.md.tmp
+    sed -i -e "1,/\\$\\$/s/\\$\\$/}] /g" $filename.md.tmp
+done
+#sed -i -e "s/\\\\begin/\\\\\\\\begin/g" $filename.md.tmp
+#sed -i -e "s/\\\\end/\\\\\\\\end/g" $filename.md.tmp
+sed -i -e "s/\\\\\$/\\\\\\\\\\\\/g" $filename.md.tmp
+sed -i -e "s/\\\\/\\\\\\\\/g" $filename.md.tmp
+cat $filename.md.tmp
+
+
+pandoc "${filename}.md.tmp" -o "${filename}.html" && sed -i -e "s/\&amp\;/\&/g" "${filename}.html"
+
+
+
