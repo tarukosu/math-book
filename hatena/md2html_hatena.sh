5129e02efed4b7916ae36d51f8ae5f19507dce54
tarukosu <u.au.sky.future12@gmail.com>
2017-02-22 20:57:49 +0900

    add new posts

---
+++ hatena/md2html_hatena.sh
@@ -0,0 +1,9 @@
+mkdir html
+../utils/md2html.sh chapter1
+../utils/md2html.sh chapter2
+../utils/md2html.sh chapter3
+../utils/md2html.sh chapter4
+../utils/md2html.sh chapter5
+../utils/md2html.sh chapter6
+
+mv *.html html/
